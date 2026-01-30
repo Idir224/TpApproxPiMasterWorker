1b69456b1b6d25d90e1337b09d07dff3ddc2e7ae
22302953 <idir.smail224@gmail.com>
2026-01-30 13:52:47 +0100

    Restructuration du projet

--- PiMasterWorker/Pi.java
+++ PiMasterWorker/Pi.java
@@ -1,13 +1,4 @@
-package assignments;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+package assignments.PiMasterWorker;
 
 /**
  * Approximates PI using the Monte Carlo method.  Demonstrates
