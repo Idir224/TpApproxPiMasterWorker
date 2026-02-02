8379ad7fba9feb50934d5a69b41a29712724257c
22302953 <idir.smail224@gmail.com>
2026-02-02 09:38:18 +0100

    Tests scalabilité forte et faible

--- PiMasterWorker/Pi.java
+++ PiMasterWorker/Pi.java
@@ -9,8 +9,18 @@ public class Pi
     public static void main(String[] args) throws Exception 
     {
 	long total=0;
-	// 10 workers, 50000 iterations each
-	total = new Master().doRun(1000000, 4);
+    //SCALABILITE FORTE ==> NB DE TIRAGES CONSTANTS
+        // p=1 total = new Master().doRun(5000000, 1); // 5 000 000 de tirages
+        // p=2 total = new Master().doRun(2500000, 2); // 5 000 000 de tirages
+        // p=4 total = new Master().doRun(1250000, 4); // 5 000 000 de tirages
+        // p=8 total = new Master().doRun(625000, 8); // 5 000 000 de tirages
+        // p=16 total = new Master().doRun(312500, 16); // 5 000 000 de tirages
+    //SCALABILITE FAIBLE ==> NB DE TIRAGES AUGMENTE AVEC LES WORKERS
+        // Premier argument FIXE (charge par worker constante)
+        //p=1 total = new Master().doRun(500000, 1);  // 500 000 tirages
+        //p=2 total = new Master().doRun(500000, 2);  // 1 000 000 de tirages
+        //p=4 total = new Master().doRun(500000, 4);  // 2 000 000 de tirages
+        //p=8 total = new Master().doRun(500000, 8);  // 4 000 000 de tirages
 	System.out.println("total from Master = " + total);
 
 
